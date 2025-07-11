cc85e2880f1779c7cf4012ffa71bf6ddcddfa601
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2025-07-11 21:45:27 +0200

    Add Prettier config and format scripts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import "./cards/person/dihor-person-card";
-import "./cards/minecraft/dihor-minecraft-card";
-import "./cards/clock/dihor-clock-card";
+import './cards/person/dihor-person-card';
+import './cards/minecraft/dihor-minecraft-card';
+import './cards/clock/dihor-clock-card';
 // Możesz dodawać kolejne komponenty bez zmian w konfiguracji
