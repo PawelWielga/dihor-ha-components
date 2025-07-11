3baa790c7c37565cc77275e3a1a1d7359dee3b83
PawelWielga <wielgapawel@gmail.com>
2025-07-11 23:44:10 +0200

    roboto

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import './cards/person/dihor-person-card';
-import './cards/minecraft/dihor-minecraft-card';
-import './cards/clock/dihor-clock-card';
+import "./cards/theme.css";
+import "./cards/core.css";
+
+import "./cards/person/dihor-person-card";
+import "./cards/minecraft/dihor-minecraft-card";
+import "./cards/clock/dihor-clock-card";
 // Możesz dodawać kolejne komponenty bez zmian w konfiguracji
